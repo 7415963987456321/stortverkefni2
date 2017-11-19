085a600b7e0646ff3c40e9c14fba97642dd6a6dd
7415963987456321 <hrs70@hi.is>
2017-11-19 23:54:24 +0000

    Byrjun á dataFetcher, sem sækir gögn ur JSON skrá

--- script-compiled.js
+++ script-compiled.js
@@ -1,3 +1,42 @@
-"use strict";
+var API_URL = '/videos.json';
+var url = '/videos.json?id=';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var body = document.querySelector('body');
+  Myndband.init(body);
+});
+
+var Myndband = function () {
+
+  function init() {
+    for (var id = 0; id < 4; id++) {
+      fetchData(id);
+    }
+  }
+
+  function fetchData(id) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (this.readyState == XMLHttpRequest.DONE) {
+        if (this.status >= 400) {
+          error('Villa kom upp');
+        } else {
+          var data = JSON.parse(request.responseText);
+          console.log(data.videos[id]);
+        }
+      }
+    };
+
+    request.onerror = function () {
+      error('Óþekkt villa');
+    };
+
+    request.open('GET', url + id, true);
+    request.send();
+  }
+  return {
+    init: init
+  };
+}();
 
 //# sourceMappingURL=script-compiled.js.map
